16d2b07f97e0c1a1ebca888f00fe1e39234b5465
Yohanna Abana <yohanaabana02@gmail.com>
2023-03-17 16:14:49 -0700

    0 - 9 again

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry function
+ * description: Prints 0 - 9
+ * Return: Alway 0
+ */
+int main(void)
+{
+	int number = 48;
+
+	while (number <= 57)
+	{
+		putchar(number);
+		if (number < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
