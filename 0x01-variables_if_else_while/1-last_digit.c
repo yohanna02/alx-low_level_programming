8a5efebb140abce6e6661c6948fb9de789a65cba
Yohanna Abana <yohanaabana02@gmail.com>
2023-03-17 15:07:31 -0700

    more if statement

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry function
+ * description: gets the last digit of number and compares it
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int last_digit = n % 10;
+
+	if (n > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+	}
+	else if (n == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	}
+	else if (n < 6 && n != 0)
+	{
+		printf("Last digit of %d is %d and is less ", n, last_digit);
+		printf("than 6 and not 0\n");
+	}
+	return (0);
+}
