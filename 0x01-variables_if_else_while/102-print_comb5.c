daf7029c93b0761eaef3b791b999c561dbe3694a
Yohanna Philip  Abana <57155932+yohanna02@users.noreply.github.com>
2023-03-18 02:11:01 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,42 +7,23 @@
  */
 int main(void)
 {
-	int number = 48;
-	int number2 = 48;
-	int number3 = 48;
-	int number4 = 49;
+	int i, j;
 
-	while (number <= 57)
-	{
-		while (number2 <= 56)
-		{
-			while (number3 <= 57)
+    	for (i = 0; i <= 98; i++)
+    	{
+        	for (j = i + 1; j <= 99; j++)
+        	{
+        		putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			if (i != 98 || j != 99)
 			{
-				while (number4 <= 57)
-				{
-					putchar(number);
-					putchar(number2);
-					putchar(' ');
-					putchar(number3);
-					putchar(number4);
-					number4++;
-					if ((number + number2 + number3 + number4) != 228)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				number3++;
-				number4 = 48;
+				putchar(',');
+				putchar(' ');
 			}
-			number2++;
-			number3 = number;
-			number4 = number2 + 1;
 		}
-		number++;
-		number2 = 48;
-		number3 = number;
-		number4 = number2 + 1;
 	}
 	putchar('\n');
 	return (0);
