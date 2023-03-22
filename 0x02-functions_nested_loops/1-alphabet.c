1d1c108fa3dd3204318532ad0b8560ed4609a642
Yohanna Abana <yohanaabana02@gmail.com>
2023-03-21 18:06:43 -0700

    fixed again

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - funtion to print a-z
+ * 
+ * Return: void
+*/
+void print_alphabet()
+{
+    int i = 97;
+
+    for(i = 97; i <= 122; i++)
+    {
+        _putchar(i);
+    }
+    _putchar('\n');
+}
