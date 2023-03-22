52792097be7d9dc3a4f742d1345d37ab5445e323
Yohanna Abana <yohanaabana02@gmail.com>
2023-03-21 20:11:49 -0700

    fixed another

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,11 +12,12 @@ int print_last_digit(int n)
 	int abs_value;
 	int last_digit;
 	
-	if (n < 0)
+	if (n == INT_MIN)
+		abs_value = INT_MAX + 1;
+	else if(n < 0)
 		abs_value = n * -1;
 	else
 		abs_value = n;
-	printf("a - %d\n", abs_value);
 	last_digit = abs_value % 10;
 	_putchar(last_digit + '0');
 
