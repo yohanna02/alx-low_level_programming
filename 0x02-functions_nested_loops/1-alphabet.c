28d854b21f02fa609d87e997cce49dcf9a899123
Yohanna Abana <yohanaabana02@gmail.com>
2023-03-21 17:32:23 -0700

    added more header files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,12 @@
 #include "main.h"
-#include "_putchar.h"
 
 /**
- * print_alphabet - funtion to print a-z
- * 
- * Return: void
+ * main - entry point
+ * description: print out _putchar
+ * Return: Always 0
 */
-void print_alphabet(void)
+int main(void)
 {
-    int i = 97;
-
-    for(i = 97; i <= 122; i++)
-    {
-        _putchar(i);
-    }
-    _putchar('\n');
+	print_alphabet();
+	return (0);
 }
