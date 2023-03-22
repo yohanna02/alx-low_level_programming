c137cb089a1aac76f04ae1620bc477a8741706c2
Yohanna Abana <yohanaabana02@gmail.com>
2023-03-22 14:47:50 -0700

    done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,59 +1,29 @@
 #include "main.h"
 
 /**
- * update - update counter
- * @n: number to update
- * Return: void
- */
-void update(int *n)
-{
-	if (*n > 98)
-		*n--;
-	else
-		*n++;
-}
-
-/**
  * print_to_98 - prints natural number to 98
  * @n: number to start counting
  * Return: void
  */
 void print_to_98(int n)
 {
-	while (n != 98)
+	if (n <= 98)
 	{
-		int tens = n / 10;
-		int more_tens = 0;
-		int ones = n % 10;
-		int temp;
-
-		if (n < 0)
+		for (; n <= 98; n++)
 		{
-			_putchar('-');
-			tens *= -1;
-			ones *= -1;
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
 		}
-		if (tens >= 10)
-		{
-			temp = tens;
-			more_tens = tens / 10;
-			tens = temp % 10;
-			if (more_tens > 0)
-			{
-				_putchar(more_tens + '0');
-			}
-			_putchar(tens + '0');
-		}
-		else if (tens > 0)
-		{
-			_putchar(tens + '0');
-		}
-		_putchar(ones + '0');
-		_putchar(',');
-		_putchar(' ');
-		update(&n);
 	}
-	_putchar('9');
-	_putchar('8');
-	_putchar('\n');
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+		}
+	}
+	printf("\n");
 }
