a24fa0c6aca087c9acf011f844500962a1acafeb
Yohanna Abana <yohanaabana02@gmail.com>
2023-03-22 02:24:51 +0100

    nested a-z

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - funtion to print a-z
+ * Return: Always return 0
+*/
+void print_alphabet(void)
+{
+    int h = 0;
+	int i = 97;
+
+    for (h = 0; h <= 10; h++)
+    {
+        for (i = 97; i <= 122; i++)
+        {
+            _putchar(i);
+        }
+        _putchar('\n');
+    }
+}
