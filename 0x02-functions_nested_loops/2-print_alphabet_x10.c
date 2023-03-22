327bf5b39e196856c92ca09de5f3cc5b9d378853
Yohanna Abana <yohanaabana02@gmail.com>
2023-03-21 18:41:39 -0700

    fixed betty's error

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet - funtion to print a-z
+ * print_alphabet_x10 - funtion to print a-z
+ * description: prints a - z using a nested loop
  * Return: Always return 0
 */
 void print_alphabet_x10(void)
