d82791ed60497066596d60507851877b606f4208
Yohanna Abana <yohanaabana02@gmail.com>
2023-03-22 12:07:28 -0700

    print to 98 fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,13 +10,25 @@ void print_to_98(int n)
 	while (n != 98)
 	{
 		int tens = n / 10;
+		int more_tens = 0;
 		int ones = n % 10;
+		int temp;
 
 		if (n < 0)
 		{
 			_putchar('-');
 			tens *= -1;
 			ones *= -1;
+		}
+		if (tens > 10)
+		{
+			temp = tens;
+			more_tens = tens / 10;
+			tens = temp % 10;
+			if (more_tens > 0)
+			{
+				_putchar(more_tens + '0');
+			}
 		}
 		if (tens > 0)
 		{
