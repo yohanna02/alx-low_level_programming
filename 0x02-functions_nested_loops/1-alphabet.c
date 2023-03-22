ccf22fd93ef468e0b8e30e181f8f9ea937ed6053
Yohanna Abana <yohanaabana02@gmail.com>
2023-03-21 18:15:17 -0700

    fiexed betty error

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,15 @@
 
 /**
  * print_alphabet - funtion to print a-z
- * 
- * Return: void
+ * Return: Always return 0
 */
-void print_alphabet()
+void print_alphabet(void)
 {
-    int i = 97;
+	int i = 97;
 
-    for(i = 97; i <= 122; i++)
-    {
-        _putchar(i);
-    }
-    _putchar('\n');
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
