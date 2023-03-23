2e9b5343e5d76cc2b2427d7907590aa418974224
Yohanna Abana <yohanaabana02@gmail.com>
2023-03-23 16:55:30 -0700

    multipy

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two number
+ * @a: first number to multipy
+ * @b: second number to multipy
+ * Return: The product of @a and @b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
