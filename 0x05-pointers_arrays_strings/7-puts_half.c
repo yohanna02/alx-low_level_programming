9bddbc5ed3d7b46cd63ff30505607c6fe88deb54
Yohanna Abana <yohanaabana02@gmail.com>
2023-03-28 11:18:32 -0700

    pusts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - puts more char
+ * @str: string to puts
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int n = 0;
+	int i = 0;
+
+	while (*(s + length) != '\0')
+	{
+		length++;
+	}
+	n = length / 2;
+	if (length % 2 != 0)
+	{
+		n = (length - 1) / 2;
+	}
+	for (i = n; i < length; i++)
+	{
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
