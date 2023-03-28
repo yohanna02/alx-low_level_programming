fec5a3a71d9fac1e7557407e3175c52a5dde635f
Yohanna Abana <yohanaabana02@gmail.com>
2023-03-28 10:52:59 -0700

    puts 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h>
+
+/**
+* puts2 - printing function
+* @str: parameter to print
+* Return: void
+*/
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i += 2;
+	}
+}
