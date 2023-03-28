732e7eca2c5858698fa5355ab1558df1ff093e20
Yohanna Abana <yohanaabana02@gmail.com>
2023-03-28 11:02:58 -0700

    fix puts 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,8 +8,14 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int length = 0;
 
-	while (*(str + i) != '\0')
+	while (*(s + length) != '\0')
+	{
+		length++;
+	}
+
+	while (i < length)
 	{
 		_putchar(*(str + i));
 		i += 2;
