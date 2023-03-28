fde8ff119dc61c0265b20db81bd4c09b09c14a3a
Yohanna Abana <yohanaabana02@gmail.com>
2023-03-28 16:05:04 -0700

    fix atio

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -21,6 +21,17 @@ int _atoi(char *s)
 	}
 	while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
 	{
+		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > 7))
+		{
+			if (sign == 1)
+			{
+				return (INT_MAX);
+			}
+			else
+			{
+				return (INT_MIN);
+			}
+		}
 		result = result * 10 + (s[i] - '0');
 		i++;
 	}
