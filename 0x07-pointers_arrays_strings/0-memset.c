2676fb39c7dcc87289258046b8adf336babfea81
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-04 11:24:02 -0700

    revert memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,11 +11,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
 
-	if (s == NULL || n <= 0)
-	{
-		return (s);
-	}
-	while (i < n)
+	while (s[i] != '\0' && i < n && b != '\0')
 	{
 		s[i] = b;
 		i++;
