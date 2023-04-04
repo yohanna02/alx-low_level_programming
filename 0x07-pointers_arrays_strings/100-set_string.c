3edb2fd2cac38afcbb17890120aa0907e8cad963
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-04 16:00:16 -0700

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets another string
+ * @s: string to set
+ * @to: where to change from
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -21,4 +21,6 @@ void print_chessboard(char (*a)[8]);
 
 void print_diagsums(int *a, int size);
 
+void set_string(char **s, char *to);
+
 #endif /* _MAIN_H_ */
