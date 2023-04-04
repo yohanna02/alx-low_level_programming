cb72d3459825b03ba7a835be29918d7a4653a245
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-04 11:56:15 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,18 @@
 #include "main.h"
 
-/
+/**
+ * _strchr - strchr function
+ * @s: string searching
+ * @c: char to search
+ * Return: NULL or pointer to s
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return s;
+		s++;
+	}
+	return NULL;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,4 +7,6 @@ char *_strchr(char *s, char c);
 
 char *_memcpy(char *dest, char *src, unsigned int n);
 
+char *_strchr(char *s, char c);
+
 #endif /* _MAIN_H_ */
