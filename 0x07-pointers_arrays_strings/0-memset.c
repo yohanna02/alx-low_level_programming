002fe75d6bfff223ef1e9f09df7ede1df2fc648e
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-04 11:18:48 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,7 +11,11 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
 
-	while (s[i] != '\0' && i < n && b != '\0')
+	if (s == NULL || n <= 0)
+	{
+		return (NULL);
+	}
+	while (i < n)
 	{
 		s[i] = b;
 		i++;
