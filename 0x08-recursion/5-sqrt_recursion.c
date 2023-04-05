ec0dd246bd6d58e6102db8a7f38594b751bbe7bd
Yohanna Philip  Abana <57155932+yohanna02@users.noreply.github.com>
2023-04-06 00:30:34 +0100

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _sqrt_recursion_helper - prints a string in reverse
+ * @n: the string to print
+ * @i: the string to print
+ * Return: int
+ */
 int _sqrt_recursion_helper(int n, int i)
 {
 	if (i * i > n)
@@ -9,6 +15,11 @@ int _sqrt_recursion_helper(int n, int i)
 	return _sqrt_recursion_helper(n, i + 1);
 }
 
+/**
+ * _sqrt_recursion - prints a string in reverse
+ * @n: the string to print
+ * Return: int
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
