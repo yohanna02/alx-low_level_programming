76f0dcf4c9eb4bfbc1f19763c5d9eb8391e923f4
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-05 16:29:11 -0700

    natural numbers

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+int _sqrt_recursion_helper(int n, int i)
+{
+	if (i * i > n)
+		return -1;
+	if (i * i == n)
+		return i;
+	return _sqrt_recursion_helper(n, i + 1);
+}
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return -1;
+	return _sqrt_recursion_helper(n, 0);
+}
