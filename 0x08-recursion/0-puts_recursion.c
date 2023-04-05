d155cf0ed48118657c25931b4e370178d89570b0
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-05 04:32:14 -0700

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - puts function with
+ * recursion
+ * @s: string to print
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	char *p = s;
+
+	if (*p)
+	{
+		_putchar(*p);
+		_puts_recursion(p + 1);
+	}
+	_putchar('\n');
+}
