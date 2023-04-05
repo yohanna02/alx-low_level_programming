04b8bf0044ba888416b2c4a3d6143e8663e16c4b
Yohanna Philip  Abana <57155932+yohanna02@users.noreply.github.com>
2023-04-06 00:53:10 +0100

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,9 +1,15 @@
 #include "main.h"
 
+/**
+ * wildcmp_helper - prints a string in reverse
+ * @s1: the string to print
+ * @s2: the string to print
+ * Return: int
+ */
 int wildcmp_helper(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
-		return 1;
+		return (1);
 	if (*s2 == '*')
 		return (wildcmp_helper(s1, s2 + 1) || (*s1 != '\0' && wildcmp_helper(s1 + 1, s2)));
 	if (*s1 != *s2)
@@ -11,6 +17,12 @@ int wildcmp_helper(char *s1, char *s2)
 	return (wildcmp_helper(s1 + 1, s2 + 1));
 }
 
+/**
+ * wildcmp - prints a string in reverse
+ * @s1: the string to print
+ * @s2: the string to print
+ * Return: int
+ */
 int wildcmp(char *s1, char *s2)
 {
 	return (wildcmp_helper(s1, s2));
