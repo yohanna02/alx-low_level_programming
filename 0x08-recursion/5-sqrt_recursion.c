6796f0a7427bad5020f92a1a2567947719fc199b
Yohanna Philip  Abana <57155932+yohanna02@users.noreply.github.com>
2023-04-06 00:31:53 +0100

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,10 +9,10 @@
 int _sqrt_recursion_helper(int n, int i)
 {
 	if (i * i > n)
-		return -1;
+		return (-1);
 	if (i * i == n)
-		return i;
-	return _sqrt_recursion_helper(n, i + 1);
+		return (i);
+	return (_sqrt_recursion_helper(n, i + 1));
 }
 
 /**
@@ -23,6 +23,6 @@ int _sqrt_recursion_helper(int n, int i)
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-		return -1;
-	return _sqrt_recursion_helper(n, 0);
+		return (-1);
+	return (_sqrt_recursion_helper(n, 0));
 }
