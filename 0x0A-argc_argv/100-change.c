a4013a9f2aaf9d727fe7a60b6714c3d9b5515321
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-09 08:39:24 -0700

    fix change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -21,34 +21,18 @@ int main(int argc, char *argv[])
 	while (1)
 	{
 		if (change == 0)
-		{
 			break;
-		}
 		if (change >= 25)
-		{
-			sum++;
 			change -= 25;
-		}
 		else if (change >= 10)
-		{
-			sum++;
 			change -= 10;
-		}
 		else if (change >= 5)
-		{
-			sum++;
 			change -= 5;
-		}
 		else if (change >= 2)
-		{
-			sum++;
 			change -= 2;
-		}
 		else if (change >= 1)
-		{
-			sum++;
 			change--;
-		}
+		sum++;
 	}
 	printf("%d\n", sum);
 	return (0);
