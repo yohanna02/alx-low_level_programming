27797634abd6539f022c6fa921f804935fbfac0f
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-11 14:11:31 -0700

    duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - count the length of a string
+ * @s: the string to count
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strcpy - copy string from one source to another
+ * @dest: where to copy the string to
+ * @src: from where to copy the string
+ * Return: point to the @dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * _strdup - duplicate string
+ * @str: string to duplicate
+ * Return: pointer to duplicated string
+ */
+char *_strdup(char *str)
+{
+	int length;
+	char *string;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	length = _strlen(str);
+	string = malloc(sizeof(char) * length);
+	_strcpy(string, str);
+	return (string);
+}
