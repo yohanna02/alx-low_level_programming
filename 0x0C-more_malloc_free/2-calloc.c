0a7b48f217201e0880520e036ac6ca4164e3ede3
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-13 19:53:18 -0700

    haaa

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,24 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-
-/**
- * _memset - memset function
- * @s: string to over ride
- * @b: char to over partd of @s
- * @n: number of @s string to over ride
- * Return: pointer to @s
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	size_t i = 0;
-
-	while (s[i] != '\0' && i < n && b != '\0')
-	{
-		s[i] = b;
-		i++;
-	}
-	return (s);
-}
+#include <string.h>
 
 /**
  * _calloc - recreating calloc
@@ -40,6 +22,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	_memset(p, 0, nmemb * size);	
+	memset(p, 0, nmemb * size);
 	return (p);
 }
