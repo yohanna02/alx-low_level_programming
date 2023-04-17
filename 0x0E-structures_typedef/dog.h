bf97acc42a9fafae61141283f330467730229a2c
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-17 16:53:08 -0700

    Dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - A dog struct
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*_DOG_H*/
