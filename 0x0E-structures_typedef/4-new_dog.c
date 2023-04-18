3ce3e7a7a02c65925e7c50206830e5edcfbb71d4
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-17 18:07:08 -0700

    new dog function

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,6 +21,23 @@ char *_strcpy(char *dest, char *src)
 }
 
 /**
+ * _strlen - count the length of a string
+ * @s: the string to count
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: Dog's name
  * @age: Dog's age
@@ -29,19 +46,29 @@ char *_strcpy(char *dest, char *src)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char n[50];
-	char o[50];
 	dog_t *dp = malloc(sizeof(dog_t));
 
 	if (dp == NULL)
 	{
 		return (NULL);
 	}
-	_strcpy(n, name);
-	_strcpy(o, owner);
+	dp->name = malloc(_strlen(name) + 1);
+	if (dp->name == NULL)
+	{
+		free(dp);
+		return (NULL);
+	}
+	_strcpy(dp->name, name);
 
-	dp->name = n;
+	dp->owner = malloc(_strlen(owner) + 1);
+	if (dp->owner == NULL)
+	{
+		free(dp->name);
+		free(dp);
+		return (NULL);
+	}
+	_strcpy(dp->owner, owner);
+
 	dp->age = age;
-	dp->owner = o;
 	return (dp);
 }
