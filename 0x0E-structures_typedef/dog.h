0d6e734c674408f630e6e9fdce61482ee23b6713
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-17 17:36:29 -0700

    typedef for dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,9 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 #endif /*_DOG_H*/
