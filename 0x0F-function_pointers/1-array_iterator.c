0a2e2fa06acac05fd1fd007c78b2a3263a6e05ce
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-19 10:36:36 -0700

    array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - loop through an array
+ * @array: array to loop through
+ * @size: size of the array
+ * @action: callback function to call
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (action)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,5 +2,6 @@
 #define _FUNCTION_POINTER_H
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /*FUNCTION_POINER_H*/
