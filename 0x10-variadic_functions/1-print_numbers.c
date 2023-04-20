c01f7dc8614a6beb43c5429ea10520d40aed95e9
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-20 12:02:15 -0700

    fixed again print number function :)

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", j);
 		if (i < n - 1 && separator != NULL)
 		{
-			printf("%s ", separator);
+			printf("%s", separator);
 		}
 	}
 	putchar('\n');
