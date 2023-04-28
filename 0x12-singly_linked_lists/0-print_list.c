3aeebfcbb24e11005a69955755ce0088e76302f7
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-28 12:48:43 +0100

    print linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,38 +11,17 @@ size_t print_list(const list_t *h)
 	int i = 0;
 	const list_t *current_list = h;
 
-	if (h == NULL)
+	if (current_list == NULL)
 	{
 		printf("[0] (nil)\n");
 		return (-1);
 	}
 
-	while (1)
+	while (current_list != NULL)
 	{
-		if (current_list->str == NULL)
-		{
-			i++;
-			printf("[0] (nil)\n");
-			if (current_list->next != NULL)
-			{
-				current_list = current_list->next;
-				continue;
-			}
-			else
-				break;
-		}
-		else
-		{
-			i++;
-			printf("[%u] %s\n", current_list->len, current_list->str);
-			if (current_list->next != NULL)
-			{
-				current_list = current_list->next;
-				continue;
-			}
-			else
-				break;
-		}
+		printf("[%d] %s\n", current_list->len, current_list->str);
+		current_list = current_list->next;
+		i++;
 	}
 	return (i);
 }
