67f201b4bc27ad22a150c744f6ab87f8d2301d6c
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-28 13:06:26 +0100

    added function to count number of nodes

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - count the number of nodes
+ * @h: Head of the linked list
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+    int i = 0;
+    const list_t *current_list = h;
+
+	while (current_list != NULL)
+	{
+		current_list = current_list->next;
+		i++;
+	}
+	return (i);
+}
