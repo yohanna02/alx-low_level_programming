7d4b0f469a7e2d9a4fb11790f23c146f4b908fd1
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-29 19:18:42 +0100

    fix betty error

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * free_listint2 - free link list
+ * @head: double pointer to head
+ * Return: void
+ */
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
