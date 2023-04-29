69ef59931e417d9c69d96b81051389ae0caf3feb
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-29 01:57:37 +0100

    free list function

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - Adds a new node at the beginning
+ * @head: pointer to the head of the linked list
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *next_node;
+
+	while (head != NULL)
+	{
+		next_node = head;
+		head = head->next;
+		free(next_node);
+	}
+}
