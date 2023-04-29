db593026455ce9e103e39e1d60129d98033a34ee
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-29 19:16:21 +0100

    free list with null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdlib.h>
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
