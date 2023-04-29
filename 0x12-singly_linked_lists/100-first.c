4bb09cc0375ab1aef180a12a4560a83b10533cbb
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-29 02:09:58 +0100

    premain function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * premain - premain function
+ */
+void __attribute__((constructor))premain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
