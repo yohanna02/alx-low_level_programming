bd761fa4e43003bbf99ff4f56ad8b6d6ef80dc20
Yohanna Abana <yohanaabana02@gmail.com>
2023-04-30 20:40:07 +0100

    fixing add node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -51,34 +51,38 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *next_list;
 	listint_t *prev_list;
-	listint_t *new_list = NULL;
+	listint_t *new_list = malloc(sizeof(listint_t));
 	unsigned int prev_index = 0;
 
-	if (head == NULL || *head == NULL)
-	{
-		return (NULL);
-	}
-	if (idx == 0)
-	{
-		prev_index = 0;
-	}
-	else
-	{
-		prev_index = idx - 1;
-	}
-	prev_list = get_nodeint_at_index(*head, prev_index);
-	if (prev_list == NULL)
-	{
-		return (NULL);
-	}
-	next_list = prev_list->next;
-	new_list = malloc(sizeof(listint_t));
 	if (new_list == NULL)
 	{
 		return (NULL);
 	}
-	prev_list->next = new_list;
-	new_list->next = next_list;
 	new_list->n = n;
-	return (new_list);
+	new_list->next = NULL;
+	if (head == NULL || *head == NULL)
+	{
+		free(new_list);
+		return (NULL);
+	}
+	if (idx == 0)
+	{
+		new_list->next = *head;
+		*head = new_list;
+		return (new_list);
+	}
+	else
+	{
+		prev_index = idx - 1;
+		prev_list = get_nodeint_at_index(*head, prev_index);
+		if (prev_list == NULL)
+		{
+			free(new_list);
+			return (NULL);
+		}
+		next_list = prev_list->next;
+		prev_list->next = new_list;
+		new_list->next = next_list;
+		return (new_list);
+	}
 }
