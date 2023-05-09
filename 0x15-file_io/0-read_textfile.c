1086bc113d6487d783337bd4d2a4aba73870710e
Yohanna Abana <yohanaabana02@gmail.com>
2023-05-09 18:31:20 +0100

    read file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - read a text file
+ * @filename: filename to read
+ * @letters: number of bytes to read
+ * Return: number of bytes read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int source_file;
+	char *buff;
+	ssize_t bytes_read;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	source_file = open(filename, O_RDONLY);
+	if (source_file == -1)
+	{
+		return (0);
+	}
+
+	buff = malloc(letters);
+	if (buff == NULL)
+	{
+		return (0);
+	}
+
+	bytes_read = read(source_file, buff, letters);
+	if (bytes_read == -1)
+	{
+		free(buff);
+		return (0);
+	}
+
+	write(STDIN_FILENO, buff, letters);
+	free(buff);
+
+	return (bytes_read);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,8 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* __MAIN_H__ */
