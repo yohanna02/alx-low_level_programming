f2516247158c3c464d08ee390649ddf54e2d4080
Yohanna Abana <yohanaabana02@gmail.com>
2023-05-09 20:30:25 +0100

    create a file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,9 @@
 
 #include <unistd.h>
 
+#define BUFFER_SIZE 1024
+
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif /* __MAIN_H__ */
