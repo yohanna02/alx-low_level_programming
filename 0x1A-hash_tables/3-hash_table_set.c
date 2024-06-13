764da8e049253ffd4432d9ae4443a7811bd66a17
Yohanna Abana <yohanaabana02@gmail.com>
2024-06-13 22:30:06 +0100

    fix insert to hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,16 +12,21 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	char new_value[strlen(value) + 1];
+	char *new_value = malloc(sizeof(char) * (strlen(value) + 1));
+	char *new_key;
 	unsigned long int index = 0;
 	hash_node_t *new_node = malloc(sizeof(hash_node_t));
 	hash_node_t *prev_node;
+	int key_len = strlen(key);
 
-	if (ht == NULL || strlen(key) == 0 || new_node == NULL)
+	if (ht == NULL || key_len == 0 || new_node == NULL)
 		return (0);
-	index = key_index(key, ht->size);
+	new_key = malloc(sizeof(char) * key_len);
+	if (new_key == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
 	strcpy(new_value, value);
-	new_node->key = key;
+	new_node->key = new_key;
 	new_node->value = new_value;
 	if (ht->array[index] != NULL)
 	{
